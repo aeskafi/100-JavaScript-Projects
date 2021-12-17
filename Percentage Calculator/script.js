680811d8004fcbd3827dfe3e4d180038f62cb19a
aeskafi <arham.eskafi@gmail.com>
2021-12-17 20:12:29 +0300

    init: Visual design was done for the project .

---
+++ Percentage Calculator/script.js
@@ -0,0 +1,15 @@
+const init = () => {
+    // let number = document.querySelector('#number');
+    // let buttons = document.querySelectorAll('button');
+
+    // buttons.forEach(function (btn) {
+    //     btn.addEventListener('click', function (e) {
+    //         var counter = number.innerText;
+    //         if (e.target.innerText === '+') counter++; else counter--;
+    //         if (e.target.innerText === 'Reset!') counter = 0;
+    //         number.innerText = counter;
+    //     })
+    // });
+}
+
+window.addEventListener('DOMContentLoaded', init)
